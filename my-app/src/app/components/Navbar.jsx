10fd804fc63d62c7273a64e73d2737df4aa6d986
Devlinshivam <iit2022158@iiita.ac.in>
2024-07-08 08:21:14 +0530

    removed some warnings from navbar.jsx file

--- my-app/src/app/components/Navbar.jsx
+++ my-app/src/app/components/Navbar.jsx
@@ -35,15 +35,15 @@ function Navbar() {
 
   return (
     <div className="py-0">
-      <header class="text-gray-400 bg-gray-900 body-font ">
-        <div class="container mx-20 flex flex-wrap p-5 flex-row justify-center items-center">
+      <header className="text-gray-400 bg-gray-900 body-font ">
+        <div className="container mx-20 flex flex-wrap p-5 flex-row justify-center items-center">
           <div className="flex title-font mx-0 font-medium items-center text-white mb-4 md:mb-0">
             <a href="/" className="flex items-center">
               <Image src="/petshop.svg" alt="background image" width={100} height={100} />
             </a>
             <HoveredLink href="/">.</HoveredLink>
           </div>
-          <nav class=" flex flex-wrap items-center mx-auto text-l justify-center">
+          <nav className=" flex flex-wrap items-center mx-auto text-l justify-center">
             <Menu >
 
               <div onClick={() => {
@@ -117,11 +117,11 @@ function Navbar() {
               onClick={toggleNotifications}
 
             >
-              <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 14 20">
+              <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 14 20">
                 <path d="M12.133 10.632v-1.8A5.406 5.406 0 0 0 7.979 3.57.946.946 0 0 0 8 3.464V1.1a1 1 0 0 0-2 0v2.364a.946.946 0 0 0 .021.106 5.406 5.406 0 0 0-4.154 5.262v1.8C1.867 13.018 0 13.614 0 14.807 0 15.4 0 16 .538 16h12.924C14 16 14 15.4 14 14.807c0-1.193-1.867-1.789-1.867-4.175ZM3.823 17a3.453 3.453 0 0 0 6.354 0H3.823Z" />
               </svg>
 
-              <div class="absolute block w-3 h-3 bg-blue-500 border-2 border-white rounded-full -top-0.5 start-2.5 dark:border-gray-900"></div>
+              <div className="absolute block w-3 h-3 bg-blue-500 border-2 border-white rounded-full -top-0.5 start-2.5 dark:border-gray-900"></div>
             </button>
 
 
@@ -134,12 +134,12 @@ function Navbar() {
                 Notifications
               </div>
               {Rows.map((row, index) => (
-                <div class="divide-y divide-gray-100 dark:divide-gray-700">
-                  <div class="w-full ps-3">
-                    <div class="text-gray-900 text-sm mb-1.5 dark:text-gray-400">
-                      <span class="font-semibold text-gray-900 dark:text-white">Pet-id:{row.pet_id}</span>: {row.work}
+                <div className="divide-y divide-gray-100 dark:divide-gray-700">
+                  <div className="w-full ps-3">
+                    <div className="text-gray-900 text-sm mb-1.5 dark:text-gray-400">
+                      <span className="font-semibold text-gray-900 dark:text-white">Pet-id:{row.pet_id}</span>: {row.work}
                     </div>
-                    <div class="text-xs text-blue-600 dark:text-blue-500">a few moments ago</div>
+                    <div className="text-xs text-blue-600 dark:text-blue-500">a few moments ago</div>
                   </div>
                 </div>
               ))}
