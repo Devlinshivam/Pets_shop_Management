b907a74c44bdf7fece3743b903ccd70ff0d1ab97
Devlinshivam <iit2022158@iiita.ac.in>
2024-07-08 13:50:17 +0530

    finally deployed

--- my-app/src/app/components/Navbar.jsx
+++ my-app/src/app/components/Navbar.jsx
@@ -34,7 +34,7 @@ function Navbar() {
   };
 
   return (
-    <div className="py-0">
+    <div className="py-0 mx-2">
       <header className="text-gray-400 bg-gray-900 body-font ">
         <div className="container mx-20 flex flex-wrap p-5 flex-row justify-center items-center">
           <div className="flex title-font mx-0 font-medium items-center text-white mb-4 md:mb-0">
@@ -108,7 +108,7 @@ function Navbar() {
             </Menu>
           </nav>
 
-          <div className="flex flex-col items-center ">
+          <div className="flex flex-col items-center  ">
             <button
               id="dropdownNotificationButton"
               data-dropdown-toggle="dropdownNotification"
@@ -117,8 +117,9 @@ function Navbar() {
               onClick={toggleNotifications}
 
             >
-              <Image src="/no.svg" alt="background image" width={20} height={20} />
-
+              <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 14 20">
+                <path d="M12.133 10.632v-1.8A5.406 5.406 0 0 0 7.979 3.57.946.946 0 0 0 8 3.464V1.1a1 1 0 0 0-2 0v2.364a.946.946 0 0 0 .021.106 5.406 5.406 0 0 0-4.154 5.262v1.8C1.867 13.018 0 13.614 0 14.807 0 15.4 0 16 .538 16h12.924C14 16 14 15.4 14 14.807c0-1.193-1.867-1.789-1.867-4.175ZM3.823 17a3.453 3.453 0 0 0 6.354 0H3.823Z" />
+              </svg>
 
               <div className="absolute block w-3 h-3 bg-blue-500 border-2 border-white rounded-full -top-0.5 start-2.5 dark:border-gray-900"></div>
             </button>
